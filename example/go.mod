9000f06cd8ff3af73c12a1089d078374152009a9
pizza-power <pizzapwr@gmail.com>
2026-02-11 12:48:50 -0600

    add example

---
+++ example/go.mod
@@ -0,0 +1,7 @@
+module example
+
+go 1.24.5
+
+require github.com/pizza-power/emojid v0.0.0
+
+replace github.com/pizza-power/emojid => ..
---
+++ example/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pizza-power/emojid"
+)
+
+func main() {
+	fmt.Println(emojid.MustNewString())
+}
